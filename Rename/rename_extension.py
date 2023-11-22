435cc8eba00aed4dc2b9c403d4e9ea07e2e45717
jsanhuo <1483104508@qq.com>
2023-11-22 22:44:45 +0800

    add rename_extension

---
+++ Rename/rename_extension.py
@@ -0,0 +1,16 @@
+import os
+def rename_files(directory, new_extension):
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            name, extension = os.path.splitext(filename)
+            if extension != new_extension:
+                new_filename = name + new_extension
+                src = os.path.join(directory, filename)
+                dst = os.path.join(directory, new_filename)
+                os.rename(src, dst)
+                print(f"Renamed file: {filename} -> {new_filename}")
+
+directory = '/path/to/directory/'
+new_extension = '.jpg'
+
+rename_files(directory, new_extension)
